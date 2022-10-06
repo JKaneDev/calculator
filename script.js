5377662da1b1164887f8505a0839da3d3609658e
James Kane <james.kane59@outlook.com>
2022-10-06 10:32:54 +0100

    Creating operate function, calculating running calc when second operator is clicked

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const calculator = {
-    displayValue: 0,
+    displayValue: '0',
     firstOperand: null,
     waitingForSecondOperand: false,
     operator: null,
 }
 const buttons = Array.from(document.querySelectorAll('.buttons')); 
+const operators = Array.from(document.querySelectorAll('.operators'));
 const clickAudio = new Audio('./sounds/clickAudio.mp3');
 
 buttons.forEach(button => button.addEventListener('click', () => clickAudio.play()));
@@ -15,28 +16,32 @@ buttons.forEach(button => button.addEventListener('click', (e) => {
     }
 
     if (target.classList.contains('operators')) {
-        console.log('operators', target.value);
+        handleOperator(target.value);
+        updateDisplay();
+        console.table(calculator);
         return;
     }
 
     if (target.classList.contains('decimal')) {
         inputDecimal(target.value);
         updateDisplay();
+        console.table(calculator);
         return;
     }
 
     if(target.classList.contains('clear')) {
-        console.log('clear', target.value);
+        console.table('clear', target.value);
         return;
     }
 
     if (target.classList.contains('delete')) {
-        console.log('delete', target.value);
+        console.table('delete', target.value);
         return;
     }
 
     inputDigit(target.value);
     updateDisplay();
+    console.table(calculator);
 }))
 
 
@@ -52,8 +57,16 @@ function clearDisplay(event, clear, display, runningCalc) {}
 function deleteDigit(event, erase, display) {}
 
 function inputDigit(digit) {
-    const { displayValue } = calculator;
-    calculator.displayValue = displayValue == '0' ? digit : displayValue + digit;
+    const { displayValue, waitingForSecondOperand } = calculator;
+    if (waitingForSecondOperand === true) {
+        calculator.displayValue = digit;
+        calculator.waitingForSecondOperand = false;
+    } else {
+        calculator.displayValue = displayValue == '0' ? digit : displayValue + digit;
+    }
+    
+
+ 
 }
 
 function inputDecimal(decimal) {
@@ -62,17 +75,43 @@ function inputDecimal(decimal) {
      }
 }
 
+function handleOperator(nextOperator) {
+    const { firstOperand, displayValue, operator } = calculator;
+    const inputValue = parseFloat(displayValue);
+    if (firstOperand == null && !isNaN(inputValue)) {
+        calculator.firstOperand = displayValue;
+    } 
+
+    calculator.waitingForSecondOperand = true;
+    calculator.operator = nextOperator;
+}
+
+function operate(firstOperand, secondOperand, operator) {
+    switch (true) {
+        case (operator === '+'):
+            return firstOperand + secondOperand;
+        case (operator === '-'):
+            return firstOperand - secondOperand;
+        case (operator === '*'):
+            return firstOperand * secondOperand;
+        case (operator === '/'):
+            return firstOperand / secondOperand;
+        default:
+            return secondOperand;
+    }
+}
+
 /* TODO:
  - DONE! Create inputDigit function, when user clicks on a digit, display value is updated
- - Create inputDecimal function, is added unless one already exists in display
- - Create handleOperator function, takes nextOperator
-    - displayValue should be stored under firstOperand
-    - operator property should be updated with whatever operator was clicked
-    - When operator is clicked, displayValue is converted to floating point 
-    - waitingForSecondOperand is set to true
+ - DONE! Create inputDecimal function, is added unless one already exists in display
+ - DONE! Create handleOperator function, takes nextOperator
+    - DONE! displayValue should be stored under firstOperand
+    - DONE! operator property should be updated with whatever operator was clicked
+    - DONE! When operator is clicked, displayValue is converted to floating point 
+    - DONE! waitingForSecondOperand is set to true
     - Whatever digits the user enters next will constitute second operand
- - Amend inputDigit: if waitingForSecondOperand = true:
-    - displayValue is overwritten with the digit that was clicked
+ - DONE! Amend inputDigit: if waitingForSecondOperand = true:
+    - DONE! displayValue is overwritten with the digit that was clicked
 - When second operand has been entered and operator is clicked:
     - Calculation thus far is evaluated and displayed (operate function)
 - Update handleOperator, check if operator has been assigned an operator
